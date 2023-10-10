abda3a4bc71c907ddabc7432ae7631de5d2699ce
Javiito32 <42683930+Javiito32@users.noreply.github.com>
2023-10-11 00:17:55 +0200

    Subprocess removed, changed debug for notice

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import asyncio
 import httpx
 import datetime
-import subprocess
+#import subprocess
 import os
 from api import CloudflareAPI
 from cloudflareLists import CloudflareLists
@@ -14,26 +14,26 @@ args = sys.argv
 
 try:
     def log(message: str):
-        if bool(os.environ.get("COMMANDS_USE_SUBPROCESS", False)):
-            subprocess.run(["echo", message])
-        else:
-            print(message)
+        #if bool(os.environ.get("COMMANDS_USE_SUBPROCESS", False)):
+        #    subprocess.run(["echo", message])
+        #else:
+        print(message)
 
     listsConfig = json.load(open('./lists.json'))
 
     _domains = {}
     domains = []
 
-    log("::debug::Fetching domains...")
+    log("::notice::Fetching domains...")
 
     #######################
     # Async fetch domains #
     #######################
     async def fetchDomain(url: str, listType: str, domains: list, _domains: dict):
         async with httpx.AsyncClient() as session:
-                log("::debug::Gettint list: " + url)
+                log("::notice::Gettint list: " + url)
                 list = await session.get(url)
-                log("::debug::List fetched: " + url)
+                log("::notice::List fetched: " + url)
 
                 for line in list.text.splitlines():
                     if not line.startswith('#') and not line == '' and not line == ' ' and not line.endswith('.'):
@@ -59,7 +59,7 @@ try:
 
     asyncio.run(fetchDomains(listsConfig['lists'], domains, _domains))
 
-    log("::debug::Done! " + str(len(domains)) + " domains fetched")
+    log("::notice::Done! " + str(len(domains)) + " domains fetched")
 
     chunks = [domains[x:x+1000] for x in range(0, len(domains), 1000)]
 
@@ -82,7 +82,7 @@ try:
 
     cloudflareAPI = CloudflareAPI(apiToken, identifier)
 
-    log("::debug::Verifying Cloudflare API token...")
+    log("::notice::Verifying Cloudflare API token...")
     get = cloudflareAPI.get('https://api.cloudflare.com/client/v4/user/tokens/verify')
     if get.status_code == 200:
         data = get.json()
@@ -93,44 +93,44 @@ try:
                     "text": "El token de Cloudflare Adblocker está a punto de caducar, por favor, renuévalo",
                     "username": "⚠️ [TOKEN RENEWAL] Cloudflare Adblockers"
                 }))
-                log("::debug::Cloudflare API token verified, but it's about to expire, please renew it")
+                log("::notice::Cloudflare API token verified, but it's about to expire, please renew it")
             else:
-                log("::debug::Cloudflare API token verified")
+                log("::notice::Cloudflare API token verified")
         else:
             log("::error file=main.py,line=79,title=Api Error::Cloudflare API token verification failed")
 
     cloudflareLists = CloudflareLists(cloudflareAPI)
     cloudflareRules = CloudflareRules(cloudflareAPI)
-    log("::debug::Cloudflare API initialized")
+    log("::notice::Cloudflare API initialized")
 
 
     adBlockingRule = cloudflareRules.getAdblockingRule()
     adBlockingRuleId = adBlockingRule['id']
-    log("::debug::Cloudflare Adblocking rule initialized")
+    log("::notice::Cloudflare Adblocking rule initialized")
 
     # Clear the rule before deleting the lists
     cloudflareRules.putRule(adBlockingRuleId, adBlockingRule)
-    log("::debug::Cloudflare Adblocking rule cleared")
+    log("::notice::Cloudflare Adblocking rule cleared")
 
     lists = cloudflareLists.getLists()
-    log("::debug::Cloudflare lists initialized")
+    log("::notice::Cloudflare lists initialized")
 
     counter = 0
-    log("::debug::Deleting Cloudflare lists")
+    log("::notice::Deleting Cloudflare lists")
     if lists is not None and len(lists) > 0:
         for list in lists:
             if list['name'].startswith('adlist_'):
                 cloudflareLists.deleteList(list['id'])
-        log("::debug::Cloudflare lists deleted")
+        log("::notice::Cloudflare lists deleted")
     else:
-        log("::debug::Cloudflare lists not found, skipping...")
+        log("::notice::Cloudflare lists not found, skipping...")
 
 
     listsIds = []
     errorLists = []
 
     counter = 0
-    log("::debug::Creating Cloudflare lists")
+    log("::notice::Creating Cloudflare lists")
     for chunk in chunks:
         try:
             listsIds.append(cloudflareLists.createList(f'adlist_{chunks.index(chunk)}', f'Adlist {chunks.index(chunk)}', chunk)['id'])
@@ -139,14 +139,14 @@ try:
             log("::group::Error creating list " + str(chunks.index(chunk)))
             log("::error::Error creating the list")
             log("::error::" + str(e))
-            log("::debug::-----------------------------------") 
-            log("::debug::" + str(chunk))
+            log("::notice::-----------------------------------") 
+            log("::notice::" + str(chunk))
             log("::endgroup::")
             pass
-    log("::debug::Cloudflare lists created")
+    log("::notice::Cloudflare lists created")
 
     cloudflareRules.putRule(adBlockingRuleId, adBlockingRule, listsIds)
-    log("::debug::Cloudflare Adblocking rule updated")
+    log("::notice::Cloudflare Adblocking rule updated")
 
     if len(errorLists) > 0:
 
@@ -178,7 +178,7 @@ try:
             "username": "✅ Cloudflare Adblockers"
         }))
 
-    log("::debug::Done!")
+    log("::notice::Done!")
 
 except Exception as e:
     
